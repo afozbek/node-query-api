ee7e25953de1f1bac0573b661b0afe1b4feff8ed
Furkan Ozbek <furkanozbek1995@gmail.com>
2019-11-10 19:47:55 +0300

    yarn ile dependecy güncellemeleri yapıldı

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const Db = require('./util/mysql');//Db bağlantıları
+const Db = require("./util/mysql"); //Db bağlantıları
 
-const body_parser = require('body-parser');
+const body_parser = require("body-parser");
 
-const userRoutes = require('./routes/auth');
+const userRoutes = require("./routes/auth");
 
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
@@ -13,24 +13,26 @@ app.use(body_parser.urlencoded({ extended: false }));
 app.use(userRoutes);
 
 app.use((error, req, res, next) => {
-    const status = error.statusCode || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({
-        message: message,
-        data: data,
-        err: error
-    });
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({
+    message: message,
+    data: data,
+    err: error
+  });
 });
 
-const config = require('./util/config');
+const config = require("./util/config");
 const db = new Db(config);
 
-db.connect().then(result => {
+db.connect()
+  .then(result => {
     console.log(result);
     app.listen(port, () => {
-        console.log(`Server listening on ${port}`);
-    })
-}).catch(err => {
+      console.log(`Server listening on ${port}`);
+    });
+  })
+  .catch(err => {
     console.log(err); //any errors including listening the server
-})
+  });
